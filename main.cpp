6deb9a8755e156492ffd384a134042c2e7085ccc
karasb1 <bohdankaras456@gmail.com>
2024-05-02 19:15:32 +0300

    The ex2 was done.

--- main.cpp
+++ main.cpp
@@ -4,5 +4,18 @@ using namespace std;
 
 int main()
 {
+	int x;
+	int y;
+	cout << "Enter x: ";
+	cin >> x;
+	cout << "Enter y: ";
+	cin >> y;
+	int s = 1;
+	int x1 = x;
+	while (s < y) {
+		x *= x1;
+		s++;
+	}
+	cout << x << endl;
 	return 0;
 }
