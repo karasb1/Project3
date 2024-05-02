00042ca73fe411013a34c320fbf1aaeee3b8ff67
karasb1 <bohdankaras456@gmail.com>
2024-05-02 19:08:18 +0300

    The ex4 was done.

--- main.cpp
+++ main.cpp
@@ -4,5 +4,17 @@ using namespace std;
 
 int main()
 {
+	int a;
+	int s = 0;
+	while (true) {
+		cout << "Enter your number: ";
+		cin >> a;
+		if (a == 0) {
+			cout << s << endl;
+			break;
+		}
+		s += a;
+		a++;
+	}
 	return 0;
 }
