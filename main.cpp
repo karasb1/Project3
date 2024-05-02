16cb0c9d12ddf3bf546fb904de0431f3da490a30
karasb1 <bohdankaras456@gmail.com>
2024-05-02 19:37:33 +0300

    The ex4 was done.

--- main.cpp
+++ main.cpp
@@ -4,5 +4,17 @@ using namespace std;
 
 int main()
 {
+	int a;
+	cin >> a;
+	int d = 1;
+	if (a < 1 || a>20) {
+		cout << "Enter number from 1 to 20";
+		return 0;
+	}
+	while (a <= 20) {
+		d *= a;
+		a++;
+	}
+	cout << d << endl;
 	return 0;
 }
