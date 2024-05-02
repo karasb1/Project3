d198a3e125310227c102c35921601b4d743dfcba
karasb1 <bohdankaras456@gmail.com>
2024-05-02 19:04:11 +0300

    The ex2 was done.

--- main.cpp
+++ main.cpp
@@ -4,5 +4,23 @@ using namespace std;
 
 int main()
 {
+	int a;
+	int b;
+	int c;
+	cout << "Enter 1 number: ";
+	cin >> a;
+	cout << "Enter 2 number: ";
+	cin >> b;
+	if (b < a) {
+		c = a;
+		a = b;
+		b = c;
+	}
+	c = 0;
+	while (a <= b) {
+		c += a;
+		a++;
+	}
+	cout << c << endl;
 	return 0;
 }
