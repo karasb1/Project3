58c0fe708c37dbaecef515f47c46012dd300f365
karasb1 <bohdankaras456@gmail.com>
2024-05-02 18:33:43 +0300

    The ex1 was done.

--- main.cpp
+++ main.cpp
@@ -4,5 +4,21 @@ using namespace std;
 
 int main()
 {
+	int a;
+	int i = 0;
+	cin >> a;
+	if (a > 0)
+	{
+		while (i <= a) {
+			cout << i << " ";
+			i++;
+		}
+	}
+	else {
+		while (i >= a) {
+			cout << i << " ";
+			i--;
+		}
+	}
 	return 0;
 }
